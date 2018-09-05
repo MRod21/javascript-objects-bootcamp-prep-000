e2ffc14c982500a068a0e8009511841fcf0cdcd4
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2018-09-05 03:31:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {artist: 'song title'}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-    return Object.assign({}, playlist, {'artistName': songTitle})
+    pl
 }
 
 function removeFromPlaylist(playlist, artistName) {
