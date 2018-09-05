f6a46114c72abc475a5524a0f5f6646e3aa06ce6
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2018-09-05 03:09:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {artist: 'song'}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-    Object.assign
+    Object.assign({} playlist, {'artistName': songTitle})
     return playlist
 }
